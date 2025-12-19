49c931bb28c8ee44bc368dd5aaf1c6828d08119d
bala-amypo <bala@amypo.in>
2025-12-19 08:52:38 +0000

    User commit

--- src/main/java/com/example/demo/dto/CropRequest.java
+++ src/main/java/com/example/demo/dto/CropRequest.java
@@ -1,5 +1,35 @@
 package com.example.demo.dto;
 
 public class CropRequest {
-    
+
+    private String name;
+    private double suitablePHMin;
+    private double suitablePHMax;
+    private double requiredWater;
+    private String season;
+
+    public CropRequest() {}
+
+    public CropRequest(String name, double suitablePHMin, double suitablePHMax, double requiredWater, String season) {
+        this.name = name;
+        this.suitablePHMin = suitablePHMin;
+        this.suitablePHMax = suitablePHMax;
+        this.requiredWater = requiredWater;
+        this.season = season;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public double getSuitablePHMin() { return suitablePHMin; }
+    public void setSuitablePHMin(double suitablePHMin) { this.suitablePHMin = suitablePHMin; }
+
+    public double getSuitablePHMax() { return suitablePHMax; }
+    public void setSuitablePHMax(double suitablePHMax) { this.suitablePHMax = suitablePHMax; }
+
+    public double getRequiredWater() { return requiredWater; }
+    public void setRequiredWater(double requiredWater) { this.requiredWater = requiredWater; }
+
+    public String getSeason() { return season; }
+    public void setSeason(String season) { this.season = season; }
 }
--- src/main/java/com/example/demo/dto/FarmRequest.java
+++ src/main/java/com/example/demo/dto/FarmRequest.java
@@ -1,5 +1,30 @@
 package com.example.demo.dto;
 
 public class FarmRequest {
-    
+
+    private String farmName;
+    private double soilPH;
+    private double waterLevel;
+    private String season;
+
+    public FarmRequest() {}
+
+    public FarmRequest(String farmName, double soilPH, double waterLevel, String season) {
+        this.farmName = farmName;
+        this.soilPH = soilPH;
+        this.waterLevel = waterLevel;
+        this.season = season;
+    }
+
+    public String getFarmName() { return farmName; }
+    public void setFarmName(String farmName) { this.farmName = farmName; }
+
+    public double getSoilPH() { return soilPH; }
+    public void setSoilPH(double soilPH) { this.soilPH = soilPH; }
+
+    public double getWaterLevel() { return waterLevel; }
+    public void setWaterLevel(double waterLevel) { this.waterLevel = waterLevel; }
+
+    public String getSeason() { return season; }
+    public void setSeason(String season) { this.season = season; }
 }
