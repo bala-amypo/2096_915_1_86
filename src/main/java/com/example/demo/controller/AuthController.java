3e80ade59f2a02a878743fd3c10248c0e6f22048
bala-amypo <bala@amypo.in>
2025-12-19 08:46:38 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,61 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.*;
+import com.example.demo.entity.User;
+import com.example.demo.security.JwtTokenProvider;
+import com.example.demo.service.UserService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
-    
+
+    private UserService userService;
+    private JwtTokenProvider jwtTokenProvider;
+
+    public AuthController(UserService userService, JwtTokenProvider jwtTokenProvider) {
+        this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest req) {
+
+        User user = User.builder()
+                .name(req.getName())
+                .email(req.getEmail())
+                .password(req.getPassword())
+                .role(req.getRole())
+                .build();
+
+        User saved = userService.register(user);
+
+        String token = jwtTokenProvider.createToken(
+                saved.getId(),
+                saved.getEmail(),
+                saved.getRole()
+        );
+
+        return ResponseEntity.ok(new AuthResponse(token, "Registered successfully"));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest req) {
+
+        User user = userService.getUserByEmail(req.getEmail());
+
+        if (!user.getPassword().equals(req.getPassword()))
+            return ResponseEntity.badRequest()
+                    .body(new AuthResponse(null, "Invalid credentials"));
+
+        String token = jwtTokenProvider.createToken(
+                user.getId(),
+                user.getEmail(),
+                user.getRole()
+        );
+
+        return ResponseEntity.ok(new AuthResponse(token, "Login successful"));
+    }
 }
