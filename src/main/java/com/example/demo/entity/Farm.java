aeff744b7ded193102bf8d63b73adcc240081d76
bala-amypo <bala@amypo.in>
2025-12-19 09:19:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/Farm.java
+++ src/main/java/com/example/demo/entity/Farm.java
@@ -1,84 +1,23 @@
+// src/main/java/com/example/demo/entity/Farm.java
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "farms")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Farm {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String farmName;
-    private double soilPH;
-    private double waterLevel;
+    private String name;
+    private Double soilPH;
+    private Double waterLevel;
     private String season;
 
     @ManyToOne
     private User owner;
-
-   
-    public Farm() {
-    }
-
-    
-    public Farm(Long id, String farmName, double soilPH, double waterLevel, String season, User owner) {
-        this.id = id;
-        this.farmName = farmName;
-        this.soilPH = soilPH;
-        this.waterLevel = waterLevel;
-        this.season = season;
-        this.owner = owner;
-    }
-
-    
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFarmName() {
-        return farmName;
-    }
-
-    public void setFarmName(String farmName) {
-        this.farmName = farmName;
-    }
-
-    public double getSoilPH() {
-        return soilPH;
-    }
-
-    public void setSoilPH(double soilPH) {
-        this.soilPH = soilPH;
-    }
-
-    public double getWaterLevel() {
-        return waterLevel;
-    }
-
-    public void setWaterLevel(double waterLevel) {
-        this.waterLevel = waterLevel;
-    }
-
-    public String getSeason() {
-        return season;
-    }
-
-    public void setSeason(String season) {
-        this.season = season;
-    }
-
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,24 +4,16 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "users")
-@Getter
-@Setter
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-
-    @Column(unique = true, nullable = false)
     private String email;
-
     private String password;
-
     private String role;
 }
