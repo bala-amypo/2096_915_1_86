93c24e7237d35babc1b139a747986d56519897d6
bala-amypo <bala@amypo.in>
2025-12-19 13:56:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+
+package com.example.demo.dto;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private Long userId;
+    private String email;
+    private String role;
+}
--- src/main/java/com/example/demo/dto/FertilizerRequest.java
+++ src/main/java/com/example/demo/dto/FertilizerRequest.java
@@ -6,22 +6,8 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class RegisterRequest {
+public class FertilizerRequest {
     private String name;
-    private String email;
-    private String password;
-}
-package com.example.demo.dto;
-
-import lombok.*;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class FarmRequest {
-    private String name;
-    private Double soilPH;
-    private Double waterLevel;
-    private String season;
+    private String npkRatio;
+    private String recommendedForCrops;
 }
