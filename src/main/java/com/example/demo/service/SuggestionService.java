9a7eb91a660e2dd4a88313dbfa8d40cab717747f
bala-amypo <bala@amypo.in>
2025-12-20 14:46:58 +0000

    User commit

--- src/main/java/com/example/demo/service/SuggestionService.java
+++ src/main/java/com/example/demo/service/SuggestionService.java
@@ -1,13 +1,4 @@
-package com.example.demo.service;
 
-import com.example.demo.entity.Suggestion;
-import java.util.List;
-
-public interface SuggestionService {
-    Suggestion generateSuggestion(Long farmId);
-    Suggestion getSuggestion(Long suggestionId);
-    List<Suggestion> getSuggestionsByFarm(Long farmId);
-}
 package com.example.demo.service;
 
 import com.example.demo.entity.Suggestion;
