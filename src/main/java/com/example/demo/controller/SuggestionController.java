b35ddb8371729838779f29f1a9c405047a36da08
bala-amypo <bala@amypo.in>
2025-12-21 07:19:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/SuggestionController.java
+++ src/main/java/com/example/demo/controller/SuggestionController.java
@@ -5,7 +5,7 @@ import com.example.demo.service.SuggestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;   // 🔑 Add this import
+import java.util.List;   
 
 @RestController
 @RequestMapping("/suggestions")
