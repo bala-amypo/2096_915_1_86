3db700c8e02c943f153aac6102014f844afaf3d6
bala-amypo <bala@amypo.in>
2025-12-23 06:18:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/SuggestionServiceImpl copy.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import com.example.demo.entity.Suggestion;
+import com.example.demo.repository.FarmRepository;
+import com.example.demo.repository.SuggestionRepository;
+import com.example.demo.service.SuggestionService;
+
+public class SuggestionServiceImpl implements SuggestionService {
+
+    private final FarmRepository farmRepository;
+    private final SuggestionRepository suggestionRepository;
+
+    public SuggestionServiceImpl(FarmRepository farmRepository,
+                                 SuggestionRepository suggestionRepository) {
+        this.farmRepository = farmRepository;
+        this.suggestionRepository = suggestionRepository;
+    }
+
+    @Override
+    public Suggestion generate(Long farmId) {
+        return suggestionRepository.save(new Suggestion());
+    }
+
+    @Override
+    public Suggestion getSuggestion(Long id) {
+        return suggestionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Suggestion not found"));
+    }
+}
